e4a973d92456d6e9063f965d4b50cf1ab915d7fe
naveen-teja2006 <singunaveen2006@gmail.com>
2026-07-22 23:46:35 +0530

    Delete BACKEND directory

--- BACKEND/routes/matchesRoutes.js
+++
@@ -1,8 +0,0 @@
-let matchController = require("../controller/matchesController");
-let seatController = require("../controller/seatsController");
-let express = require("express");
-let router = express.Router();
-router.get("/",matchController.getAllMatches)
-router.get("/:id",matchController.getAllMatchesById);
-router.get("/:id/seats",seatController.getAllSeatsById);
-module.exports = router;
--- BACKEND/routes/registerRoutes.js
+++
@@ -1,5 +0,0 @@
-let registerController = require("../controller/registerController.js");
-let express = require("express");
-let router  = express.Router();
-router.post("/",registerController.insertUsers);
-module.exports = router;
